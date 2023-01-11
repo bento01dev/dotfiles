47c8185bc184ccbdb05ea0a68c79e2375df12845
Akilesh Sethu <asethu19@gmail.com>
2023-01-11 21:56:28 +0800

    toggle term

---
+++ nvim/lua/aki/toggleterm.lua
@@ -0,0 +1,11 @@
+require('toggleterm').setup {
+    open_mapping = [[<c-\>]],
+    hide_numbers = true,
+    auto_scroll = true,
+    direction = 'float',
+    float_opts = {
+        border = 'curved',
+        width = 150,
+        height = 40,
+    },
+}
