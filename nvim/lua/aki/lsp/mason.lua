204bbee97374f92110f2111746dcc9e1a174f51b
Akilesh Sethu <asethu19@gmail.com>
2023-06-24 21:55:59 +0800

    nvim 0.8.4

--- nvim/lua/aki/lsp/mason.lua
+++ nvim/lua/aki/lsp/mason.lua
@@ -9,7 +9,7 @@ require("mason").setup({
 })
 
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "pyright", "rnix" },
+    ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "pyright", "rnix", "buf" },
     automatic_installation = true,
 })
 
@@ -50,6 +50,10 @@ require("lspconfig").clangd.setup {
     capabilities = require("aki.lsp.handlers").capabilities,
 }
 require("lspconfig").rnix.setup {
+    on_attach = require("aki.lsp.handlers").on_attach,
+    capabilities = require("aki.lsp.handlers").capabilities,
+}
+require("lspconfig").buf.setup {
     on_attach = require("aki.lsp.handlers").on_attach,
     capabilities = require("aki.lsp.handlers").capabilities,
 }
